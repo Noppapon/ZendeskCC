a1125bdc4213508a18bfb72205cdd8373e1c89de
Noppapon <76277934+Noppapon@users.noreply.github.com>
2021-11-28 16:32:17 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,32 @@
 import unittest
+from Main import main, menu
+import sys
+from contextlib import contextmanager
+from io import StringIO
 
 class TestSum(unittest.TestCase):
 
-    def test_sum(self):
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+    @contextmanager
+    def captured_output(self):
+        new_out, new_err = StringIO(), StringIO()
+        old_out, old_err = sys.stdout, sys.stderr
+        try:
+            sys.stdout, sys.stderr = new_out, new_err
+            yield sys.stdout, sys.stderr
+        finally:
+            sys.stdout, sys.stderr = old_out, old_err
 
-    def test_sum_tuple(self):
-        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+    def test_auth_error(self):
+        with captured_output() as (out, err):
+            main('zccwrongsubdomain', 'wrongemail', 'wrong password')
+        output = out.getvalue().strip()
+        self.assertEqual(output, "Authentication failed, terminating the program...\n")
+
+    def test_auth_success(self):
+        with captured_output() as (out, err):
+            main('zcccorrectsubdomain', 'correctmail', 'correct password')
+        output = out.getvalue().strip()
+        self.assertEqual(output, "Success!\n")
 
 if __name__ == '__main__':
     unittest.main()
